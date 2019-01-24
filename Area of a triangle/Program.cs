dea2d53caa12e94bc282398305578fa3a3584436
cicley lee <cicley.lee@gmail.com>
2019-01-24 14:07:46 -0500

    Add project files.

---
+++ Area of a triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_a_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s, area;
+            double a, b, c;
+
+
+            Console.WriteLine("Side #1");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Side #2");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Side #3");
+            c = double.Parse(Console.ReadLine());
+
+            s = (a + b + c) / 2;
+            area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+
+            Console.WriteLine("The area is {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
